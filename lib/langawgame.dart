31945e1d1c23faf451fb68b03fc04f4dcac087f7
sselvanathan <senthuranselvanathanbusiness@gmail.com>
2019-09-09 20:44:38 +0200

    Initial Commit

---
+++ lib/langawgame.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+import 'package:flame/game.dart';
+
+class LangawGame extends Game {
+  Size screenSize;
+  double tileSize;
+
+  void render(Canvas canvas) {
+    Rect bgRect = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
+    Paint bgPaint = Paint();
+    bgPaint.color = Color(0xff576574);
+    canvas.drawRect(bgRect, bgPaint);
+  }
+
+  void update(double t) {}
+
+  void resize(Size size) {
+    screenSize = size;
+    tileSize = screenSize.width / 9;
+  }
+}
