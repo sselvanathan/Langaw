6d7da523c881448e41a48cb6d49c481dc8b35182
sselvanathan <senthuranselvanathanbusiness@gmail.com>
2019-09-15 15:24:55 +0200

    Initialize Flies

--- lib/langawgame.dart
+++ lib/langawgame.dart
@@ -1,30 +1,37 @@
 import 'dart:ui';
 import 'package:flame/game.dart';
 import 'package:flame/flame.dart';
+import 'package:langaw/components/fly.dart';
 
 class LangawGame extends Game {
   Size screenSize;
   double tileSize;
-
-  void render(Canvas canvas) {
-    Rect bgRect = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
-    Paint bgPaint = Paint();
-    bgPaint.color = Color(0xff576574);
-    canvas.drawRect(bgRect, bgPaint);
-  }
-
-  void update(double t) {}
-
-  void resize(Size size) {
-    screenSize = size;
-    tileSize = screenSize.width / 9;
-  }
+  List<Fly> flies;
 
   LangawGame() {
     initialize();
   }
 
   void initialize() async {
+    flies = List<Fly>();
     resize(await Flame.util.initialDimensions());
+  }
+
+  void render(Canvas canvas) {
+    Rect bgRect = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
+    Paint bgPaint = Paint();
+    bgPaint.color = Color(0xff576574);
+    canvas.drawRect(bgRect, bgPaint);
+
+    flies.forEach((Fly fly) => fly.render(canvas));
+  }
+
+  void update(double t) {
+    flies.forEach((Fly fly) => fly.update(t));
+  }
+
+  void resize(Size size) {
+    screenSize = size;
+    tileSize = screenSize.width / 9;
   }
 }
